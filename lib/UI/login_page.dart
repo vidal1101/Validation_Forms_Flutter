25678dd7aec1519534877134ad59ffbbd2d6806b
vidal1101 <rodrigovidal278@gmail.com>
2022-06-27 16:35:35 -0600

    creacion del diseño del formulario con la decoracion reutilizable y listo para empezar la validacion

--- lib/UI/login_page.dart
+++ lib/UI/login_page.dart
@@ -8,23 +8,67 @@ class LoginPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: BackGroundScreen(
-        childbody: childbodycard(),
+        childbody: childbodycard(context),
         ),
     );
   }
 
-  Widget childbodycard (){
+  Widget childbodycard (BuildContext context){
     return SingleChildScrollView(
       child: Column(
         children: [
-          SizedBox(height: 220,),
+          SizedBox(height: 200,),
           CardContainer(
-            childcard: Text('Hola mundo')
+            childcard: Column(
+              children: [
+                SizedBox(height: 10,),
+                Text('Login' , style: Theme.of(context).textTheme.headline4 ),
+                SizedBox(height: 20,),
+                _Loginforms(),
+              ],
+            )
           ),
+          SizedBox(height: 30,),
+          Text('Create a new account', style:  Theme.of(context).textTheme.headline6,)
         ],
       ),
     );
   }
 
-  
+}
+
+class _Loginforms extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Form(
+        // TODO: mantener la  referencia del KEY 
+        child: Column(
+          children: [
+            TextFormField(
+              autocorrect: false,
+              keyboardType: TextInputType.emailAddress,
+              decoration: InputDecorationLogin.getinputDecorationlogin(
+                hintText: 'Example@gmail.com',
+                labelText: 'Email',
+                prefixIcon: Icons.alternate_email_rounded,
+              ),
+            ),
+            SizedBox(height: 15,),
+            TextFormField(
+              autocorrect: false,
+              keyboardType: TextInputType.visiblePassword,
+              decoration: InputDecorationLogin.getinputDecorationlogin(
+                hintText: 'your password',
+                labelText: 'Password',
+                prefixIcon: Icons.lock_outline_rounded,
+              ),
+            ),
+            SizedBox(height: 30,),
+          ],
+        )
+      ),
+    );
+  }
 }
--- lib/widgets/widgets.dart
+++ lib/widgets/widgets.dart
@@ -1,3 +1,4 @@
 
+export 'package:validation_form/widgets/inputDecoration.dart';
 export 'package:validation_form/widgets/cardContainer.dart';
 export 'package:validation_form/widgets/background.dart';
