2bf0a62da2c3731799cba0bafc9c71a1e40ecf14
vidal1101 <rodrigovidal278@gmail.com>
2022-06-27 11:58:22 -0600

    paqueteria basica para iniciar el proyecto

---
+++ android/app/src/main/kotlin/com/example/validation_form/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.validation_form
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:validation_form/UI/app_main.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+
+
