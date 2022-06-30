b2494d54af4a88a00897895c6b5eaff3e1c4cf73
vidal1101 <rodrigovidal278@gmail.com>
2022-06-30 00:02:40 -0600

    recuperar el usuario y guardalo y demas detalles

--- lib/UI/home_page.dart
+++ lib/UI/home_page.dart
@@ -1,13 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:validation_form/services/services.dart';
 
 
 class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final userservice = Provider.of<AuthLoginFirebase>(context);
+
     return Scaffold(
+      appBar: AppBar(
+        title: Text('Products'),
+      ),
       body: Center(
-        child: Text('Hola Mundo'),
+        child: Text('Hello World'),
      ),
    );
   }
