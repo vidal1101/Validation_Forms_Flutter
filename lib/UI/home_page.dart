dc6e60d16410e04d24cefb6903b1a87851a39a8d
vidal1101 <rodrigovidal278@gmail.com>
2022-07-04 00:11:38 -0600

    creacion del ejemplo con debouncer

--- lib/UI/home_page.dart
+++ lib/UI/home_page.dart
@@ -1,9 +1,17 @@
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
+import 'package:provider/provider.dart';    
 import 'package:validation_form/services/services.dart';
+import 'package:validation_form/widgets/widgets.dart';
 
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  String? valueText= '';
 
   @override
   Widget build(BuildContext context) {
@@ -21,23 +29,77 @@ class HomePage extends StatelessWidget {
              icon: Icon(Icons.login_rounded) ),
         ],
       ),
-      body:  Center(
-        child: FutureBuilder(
+      body:  FutureBuilder(
           future: userservice.readInformationUser(),
           builder: (BuildContext context , AsyncSnapshot<String> snapshot){
             if(snapshot.hasData){
               print(snapshot.data.toString());
             }
-            return Column(
-              children: [
-                SizedBox(height: 50,),
-                Text('Hello'),
-                Text(snapshot.data.toString().toUpperCase()),
-              ],
+            return SingleChildScrollView(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  SizedBox(height: 50,),
+                  Text('Hello'),
+                  Text(snapshot.data.toString().toUpperCase()),
+                  SizedBox(height: 50,),
+                  Padding(
+                    padding: const EdgeInsets.all(15.0),
+                    child: textFieldDebouncer(),
+                  ),
+                  SizedBox(height: 25,),
+                  paintText(),
+                  SizedBox(height: 25,),
+
+                ],
+              ),
             );
 
           }),
-     ),
    );
   }
+
+  Widget textFieldDebouncer(){
+    return Container(
+      child: TextFormField(
+              autocorrect: false,
+              keyboardType: TextInputType.text,
+              decoration: InputDecorationLogin.getinputDecorationlogin(
+                hintText: 'Search',
+                labelText: 'Search',
+                prefixIcon: Icons.search_rounded,
+              ),
+              onChanged: (value){
+
+                Debouncer.runDebouncer( (){
+                  //peticion http o cualquier proceso de busqueda con el debouncer
+                  setState(() {
+                    valueText = value;
+                    print(value);
+                    paintText(); //si fuera la peticion http 
+                  });
+                  
+                  
+                });
+  
+              },
+            ),
+    );
+  }
+
+  Widget paintText(){
+    return Container(
+      child: FutureBuilder(
+        builder: (BuildContext context, AsyncSnapshot snapshot){
+          if(valueText == ''){
+            return const CircularProgressIndicator();
+          }else{
+            return Text(valueText.toString());
+          }
+        }
+      ),
+    );
+  }
+
+  
 }
--- lib/services/services.dart
+++ lib/services/services.dart
@@ -1,6 +1,7 @@
 
 export 'package:validation_form/services/user_secure_storage.dart';
 export 'package:validation_form/services/auth_login.dart';
+export 'package:validation_form/services/debouncer.dart';
 
 
 
